69b40ecff19b436fbc0c76ec8c948b338418a42b
Michael Ellis <michaelellis003@gmail.com>
2025-07-11 15:46:25 -0500

    resolve imports for pyright

--- lm/tokenizer/__init__.py
+++ lm/tokenizer/__init__.py
@@ -4,6 +4,7 @@ This package provides tokenization utilities, including a naive tokenizer
 implementation.
 """
 
+from .bpe import BPETokenizer
 from .naive import NaiveTokenizer
 
-__all__ = ['NaiveTokenizer']
+__all__ = ['NaiveTokenizer', 'BPETokenizer']
